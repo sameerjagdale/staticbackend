6455808e6b2b1953fdfccf4416272e4205cf0654
Sameer Jagdale <sameer.jagdale@mail.mcgill.ca>
2013-04-21 19:05:27 -0400

    matrix operations sum and multiplication added for float  and integer

--- src/matrixOps.cpp
+++ src/matrixOps.cpp
@@ -1,15 +1,53 @@
 #include <iostream>
 
-int* matrixMult(int* a, int *b, int row1, int column1, int column2) {
+int* matrixMult_int(int* a, int *b, int row1, int column1, int column2) {
 	int *c = new int[row1 * column2];
 
 	for (int i = 0; i < row1; i++) {
 		for (int j = 0; j < column2; j++) {
 			int sum = 0;
 			for (int k = 0; k < column1; k++) {
-				sum += *(a + i * row1 + j) * (*(b + i * column1 + column2));
+				sum += *(a + i * column1 + k) * (*(b + k * column2 + j));
 			}
-			*(c + i * row1 + column2) = sum;
+			*(c + i * column2 + j) = sum;
+		}
+	}
+	return c;
+}
+
+int* matrixSum_int(int *a, int *b, int row, int column) {
+	int *c = new int[row * column];
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < column; j++) {
+			*(c + i * column + j) = *(a + i * column + j)
+					+ *(b + i * column + j);
+		}
+	}
+	return c;
+}
+
+float* matrixMult_float(float* a, float *b, int row1, int column1,
+		int column2) {
+	float *c = new float[row1 * column2];
+
+	for (int i = 0; i < row1; i++) {
+		for (int j = 0; j < column2; j++) {
+			int sum = 0;
+			for (int k = 0; k < column1; k++) {
+				sum += *(a + i * column1 + k) * (*(b + k * column2 + j));
+			}
+			*(c + i * column2 + j) = sum;
+		}
+	}
+	return c;
+}
+
+float* matrixSum_float(float *a, float *b, int row, int column) {
+	float *c = new float[row * column];
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < column; j++) {
+			*(c + i * column + j) = *(a + i * column + j)
+					+ *(b + i * column + j);
 		}
 	}
 	return c;
