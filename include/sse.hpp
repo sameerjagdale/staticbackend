fd4215dcc9cf4cba12eebdc78d0b5289fdaffd98
Sameer Jagdale <sameer.jagdale@mail.mcgill.ca>
2013-04-22 21:12:02 -0400

    sse functions modified to handle double precision instead single precision

--- include/sse.hpp
+++ include/sse.hpp
@@ -1,6 +1,6 @@
 #include <immintrin.h>
 #include<stdio.h>
-float* sse_add(float*, float*, float*, int size);
-float* sse_sub(float*, float*, float*, int size);
-float* sse_mul(float*, float*, float*, int size);
+double* sse_add(double*, double*, double*, int size);
+double* sse_sub(double*, double*, double*, int size);
+double* sse_mul(double*, double*, double*, int size);
 
--- src/sse.cpp
+++ src/sse.cpp
@@ -1,84 +1,84 @@
 #include"sse.hpp"
 /*int main(){
-	
-float a[16],b[16],*c;
-int i;
-for(i=0;i<16;i++){
-	b[i]=1;
-	a[i]=1;
-	printf("\n%f",a[i]);
-}
 
-c=sse_add(a,b,16);
-for(i=0;i<16;i++){
-	printf("%f\n",c[i]);
-}
-return 0;
-}
-*/
-float* sse_add(float  *a, float *b,int size){
-	
-	float *c;
-	c=new float[size];
-	for(int i=0;i<(size/8)*8;i+=8){
-		__m128 sse_a =_mm_load_ps(&a[i]);
-		__m128 sse_b =_mm_load_ps(&b[i]);
-		__m128 sse_c =_mm_add_ps(sse_a,sse_b);
-		_mm_store_ps(&c[i],sse_c);
+ float a[16],b[16],*c;
+ int i;
+ for(i=0;i<16;i++){
+ b[i]=1;
+ a[i]=1;
+ printf("\n%f",a[i]);
+ }
+
+ c=sse_add(a,b,16);
+ for(i=0;i<16;i++){
+ printf("%f\n",c[i]);
+ }
+ return 0;
+ }
+ */
+
+double* sse_add(double *a, double *b, int size) {
+
+	double *c;
+	c = new double[size];
+	for (int i = 0; i < (size / 2) * 2; i += 2) {
+		__m128d sse_a = _mm_load_pd(&a[i]);
+		__m128d sse_b = _mm_load_pd(&b[i]);
+		__m128d sse_c = _mm_add_pd(sse_a, sse_b);
+		_mm_store_pd(&c[i], sse_c);
 	}
-	for(int i=(size/8)*8;i<size;i++){
-		c[i]=a[i]+b[i];
+	for (int i = (size / 2) * 2; i < size; i++) {
+		c[i] = a[i] + b[i];
 	}
 	return c;
 }
 
-float* sse_sub(float *a,float *b,int size){
-	float *c;
-	c= new float[size];
-	for(int i=0;i<(size/4)*4;i++){
-		 __m128 sse_a =_mm_load_ps(&a[i]);
-                 __m128 sse_b =_mm_load_ps(&b[i]);
-                 __m128 sse_c =_mm_sub_ps(sse_a,sse_b);
-                 _mm_store_ps(&c[i],sse_c);
-	
-	}
-	for(int i=(size/4)*4;i<size;i++){
-                c[i]=a[i]-b[i];
-        }
-	return c ;
-}
+double* sse_sub(double *a, double *b, int size) {
+	double *c;
+	c = new double[size];
+	for (int i = 0; i < (size / 2) * 2; i += 2) {
+		__m128d sse_a = _mm_load_pd(&a[i]);
+		__m128d sse_b = _mm_load_pd(&b[i]);
+		__m128d sse_c = _mm_sub_pd(sse_a, sse_b);
+		_mm_store_pd(&c[i], sse_c);
 
-float* sse_mul(float*a,float*b,int size){
-	 float *c;
-        c= new float[size];
-        for(int i=0;i<(size/4)*4;i++){
-                 __m128 sse_a =_mm_load_ps(&a[i]);
-                 __m128 sse_b =_mm_load_ps(&b[i]);
-                 __m128 sse_c =_mm_mul_ps(sse_a,sse_b);
-                 _mm_store_ps(&c[i],sse_c);
-		
-        }
-        for(int i=(size/4)*4;i<size;i++){
-                c[i]=a[i]*b[i];
-        }
+	}
+	for (int i = (size / 2) * 2; i < size; i++) {
+		c[i] = a[i] - b[i];
+	}
 	return c;
 }
 
+double* sse_mul(double*a, double*b, int size) {
+	double *c;
+	c = new double[size];
+	for (int i = 0; i < (size / 2) * 2; i += 2) {
+		__m128d sse_a = _mm_load_pd(&a[i]);
+		__m128d sse_b = _mm_load_pd(&b[i]);
+		__m128d sse_c = _mm_mul_pd(sse_a, sse_b);
+		_mm_store_pd(&c[i], sse_c);
 
-float* sse_div(float*a,float*b,int size){
-	  float *c;
-        c= new float[size];
-        for(int i=0;i<(size/4)*4;i++){
-                 __m128 sse_a =_mm_load_ps(&a[i]);
-                 __m128 sse_b =_mm_load_ps(&b[i]);
-                 __m128 sse_c =_mm_div_ps(sse_a,sse_b);
-                 _mm_store_ps(&c[i],sse_c);
+	}
+	for (int i = (size / 2) * 2; i < size; i++) {
+		c[i] = a[i] * b[i];
+	}
+	return c;
+}
 
-        }
-        for(int i=(size/4)*4;i<size;i++){
-                c[i]=a[i]*b[i];
-        }
-        return c;
+double* sse_div(double*a, double*b, int size) {
+	double *c;
+	c = new double[size];
+	for (int i = 0; i < (size / 2) * 2; i++) {
+		__m128d sse_a = _mm_load_pd(&a[i]);
+		__m128d sse_b = _mm_load_pd(&b[i]);
+		__m128d sse_c = _mm_div_pd(sse_a, sse_b);
+		_mm_store_pd(&c[i], sse_c);
+
+	}
+	for (int i = (size / 2) * 2; i < size; i++) {
+		c[i] = a[i] * b[i];
+	}
+	return c;
 
 }
 
